98c132a3a6b97f0999be32fcc3f30be7287f628d
Raul Juliao <raulj@uninorte.edu.co>
2020-10-26 13:34:01 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Routes from './Routes/Routes';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+import theme from './theme';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const browserHistory = createBrowserHistory();
+
+export default class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Router history={browserHistory}>
+          <CssBaseline />
+          <Routes />
+        </Router>
+      </ThemeProvider>
+    );
+  }
 }
 
-export default App;
